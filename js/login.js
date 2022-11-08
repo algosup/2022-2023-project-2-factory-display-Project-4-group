bfb4472c96f185713dd4e1ad104b9476321ba0da
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-08 16:20:10 +0100

    edit of login page

--- js/login.js
+++ js/login.js
@@ -4,11 +4,10 @@ function validate() {
    var username = document.getElementById("username").value;
    var password = document.getElementById("password").value;
    if (username == "admin" && password == "admin") {
-       alert ("Login successfully");
-         window.location = "success.html"; // Redirecting to other page.
+      window.location.href ="settings.html" // Redirecting to other page.
    }
    else {
        alert("Invalid username or password");
-        return false;
+      return false;
    }
 }
