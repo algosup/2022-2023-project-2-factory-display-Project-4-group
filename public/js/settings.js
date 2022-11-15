c6445504c8b7e80a861e0c8dee9dc96d1b693408
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-15 16:08:19 +0100

    edit of settings.html

---
+++ public/js/settings.js
@@ -0,0 +1,9 @@
+let html = document.getElementsByTagName('html');
+let radios = document.getElementsByName('themes'); 
+      
+for (i = 0; i < radios.length; i++) {
+    radios[i].addEventListener('change', function() {
+    html[0].classList.remove(html[0].classList.item(0));
+        html[0].classList.add(this.id);
+    });
+}
