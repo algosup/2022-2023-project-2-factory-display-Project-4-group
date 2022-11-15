4c3cbb67c341399063ee3e28bd3e33c9d5f1219e
laurentbouquin <laurent.bouquin@algosup.com>
2022-11-15 15:02:00 +0100

    Changing indexing of the files
    placing the files in the public folder and adding the firebase connection.

---
+++ public/js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDOLs8HAI5mvU4W_8bZ9UkeukOzZsZSNgs",
+    authDomain: "portfolio-7cb4c.firebaseapp.com",
+    databaseURL: "https://portfolio-7cb4c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "portfolio-7cb4c",
+    storageBucket: "portfolio-7cb4c.appspot.com",
+    messagingSenderId: "497691677894",
+    appId: "1:497691677894:web:d2bffecff07a15e17573c7"
+};
+firebase.initializeApp(firebaseConfig);
