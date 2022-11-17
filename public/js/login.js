a1a82e4b8e494b2ec669444e7d38592da881af50
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-17 14:48:44 +0100

    update login

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ function validate() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 	if (username == "admin" && password == "admin") {
-	   self.location ="settings.html";
+	   window.location.assign("settings.html");
 	   alert("Login successfully");
 	}
 	else {
