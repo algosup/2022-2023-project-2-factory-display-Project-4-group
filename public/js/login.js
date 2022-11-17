80830e54ce3251f227fe7989fb4bd3c4fd1b0346
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-17 14:30:32 +0100

    change login

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,12 @@
-// add Obfuscated code here
-
 function validate() {
-   var username = document.getElementById("username").value;
-   var password = document.getElementById("password").value;
-   if (username == "admin" && password == "admin") {
-      window.location.href ="settings.html";
-      alert("Login successfully");
-   }
-   else {
-       alert("Invalid username or password");
-      return false;
-   }
-}
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	if (username == "admin" && password == "admin") {
+	   self.location ="settings.html";
+	   alert("Login successfully");
+	}
+	else {
+		alert("Invalid username or password");
+	   return false;
+	}
+ }
