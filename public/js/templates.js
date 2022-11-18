b2df5b7486cd4a99a899ba677778645b9b5a23ef
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-18 16:38:09 +0100

    edit of grid + first look of functions

--- public/js/templates.js
+++ public/js/templates.js
@@ -4,21 +4,41 @@ var container = document.getElementById('display');
 for (let index = 0; index < 48; index++) {
 	var grid = document.createElement('div');
 	grid.classList.add('grid');
-	grid.className = 'square';
-	grid.id = 1;
+	grid.className = `square1`;
+	grid.id = index;
 	grid.setAttribute('onclick', 'changeColor(this)');
 	container.appendChild(grid);
-
-
-	
 }
 
 function changeColor(element) {
-	if (element.id == 1) {
-		element.id = 2;
-		element.style.backgroundColor = 'red';
-	} else if (element.id == 2) {
-		element.id = 1;
-		element.style.backgroundColor = '#d2d2d2';
+	if (element.className == 'square1') {
+		element.className = 'square2';
+	} else if (element.className == 'square2') {
+		element.className = 'square1';
 	}
 }
+
+// Comment bloquer les cases ?
+// Si, détection d'une case avec square2, alors seules les cartes avec un id égal à
+// id + 1, id - 1, id + 8, id - 8 peuvent être cliquées
+
+// Essai d'une fonction qui vérifie si une case est déjà cliquée
+// function isDifferent() {
+// 	var grid = document.getElementsByClassName('grid');
+// 	for (i = 0; i < grid.length; i++) {
+// 		if (grid[i].className == 'square2') {
+// 			return grid[i].id;
+// 		}
+// 	}
+// }	
+
+
+// Essai d'une fonction qui bloque les cases
+// function block() {
+// 	var grid = document.getElementsByClassName('grid');
+// 	for (i = 0; i < grid.length; i++) {
+// 		if (grid[i].id == isDifferent() + 1 || grid[i].id == isDifferent() - 1 || grid[i].id == isDifferent() + 8 || grid[i].id == isDifferent() - 8) == false {
+// 			grid[i].setAttribute('onclick', 'null');
+// 		}
+// 	}
+// }
