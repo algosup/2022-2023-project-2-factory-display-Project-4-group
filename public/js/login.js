674a9be9941e8e0b34268c47e048f64d4c1b955e
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-20 14:05:47 +0100

    create compatibility mac / windows

--- public/js/login.js
+++ public/js/login.js
@@ -2,11 +2,14 @@ function validate() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 	if (username == "admin" && password == "admin") {
-	   window.location.assign("settings.html");
-	   alert("Login successfully");
-	}
-	else {
-		alert("Invalid username or password");
+		if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+			login.href='settings.html';
+			alert("Login successfully");
+		} else {
+			window.location.assign("settings.html");
+	   	alert("Login successfully");
+	}} else {
+	alert("Invalid username or password");
 	   return false;
 	}
  }
