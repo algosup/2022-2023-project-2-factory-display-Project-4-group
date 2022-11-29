b2c1095c1d437edb853260f5f4a35702e3272a18
laurentbouquin <laurent.bouquin@algosup.com>
2022-11-29 16:35:11 +0100

    Updating templates page to select the pattern.

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,32 +1,73 @@
 
 var container = document.getElementById('display');
+var container2 = document.getElementById('display2');
 let squares = 0;
 let last = 0;
-
+let rot = 0;
+let element = document.getElementById('sizes');
 
 for (let index = 1; index <= 48; index++) {
 	var grid = document.createElement('div');
 	grid.classList.add('grid');
-	grid.className = `square1`;
+	if (index == 1){
+		grid.className = 'square2';
+	} else {
+		grid.className = `square1`;
+	}
 	grid.id = index;
-	grid.setAttribute('onclick', 'changeColor(this)');
 	container.appendChild(grid);
+}
+for (let index = 49; index <= 96; index++) {
+	var gridA = document.createElement('div');
+	gridA.className = `squareT`;
+	gridA.id = index;
+	container2.appendChild(gridA);
 }
 
 // Display the selected template size
 function displayTemplate(){
-	let element = document.getElementById('sizes');
 	var rows = element.options[element.selectedIndex].value[0];
 	var columns = element.options[element.selectedIndex].value[2];
 	console.log(rows, columns);
 	displaySelected(rows, columns);
+	if (element.options[element.selectedIndex].value[0] == element.options[element.selectedIndex].value[2] || element.options[element.selectedIndex].value[2] == 8){
+		let rotate = document.getElementById('rotate');
+		rotate.style.backgroundColor = 'gray';
+		rotate.style.cursor = 'not-allowed';
+		rotate.setAttribute('onclick', '');
+	}
+	else {
+		let rotate = document.getElementById('rotate');
+		rotate.style.backgroundColor = '#ff6600';
+		rotate.style.cursor = 'pointer';
+		rotate.setAttribute('onclick', 'rotateTemplate()');
+	}
 }
 
+function rotateTemplate(){
+	let element = document.getElementById('sizes');
+	if (rot == 0 && element.options[element.selectedIndex].value[2] != 8){
+		var rows = element.options[element.selectedIndex].value[2];
+		var columns = element.options[element.selectedIndex].value[0];
+		console.log(rows, columns);
+		displaySelected(rows, columns);
+		rot = 1;
+	}
+	else if (rot == 1 && element.options[element.selectedIndex].value[2] != 8){
+		var rows = element.options[element.selectedIndex].value[0];
+		var columns = element.options[element.selectedIndex].value[2];
+		console.log(rows, columns);
+		displaySelected(rows, columns);
+		rot = 0;
+	}
+}
 
 function displaySelected(rows, columns){
 	for (let index = 1; index <= 48; index++) {
 		var grid = document.getElementById(index);
-		grid.className = `square1`;
+		if (grid.className == 'square2'){
+			grid.className = `square1`;
+		}
 	}
 	var count = 1;
 	for (let rowsIndex = 1; rowsIndex <= rows; rowsIndex++) {
@@ -40,71 +81,63 @@ function displaySelected(rows, columns){
 	}
 }
 
-function changeColor(element) {
-	if (element.className == 'square1') {
-		if (squares > 0) {
-			// console.log(element.id, last, squares, element.className);
-			if (element.id == (last -(-1))){
-				console.log(+1);
-				element.className = 'square2';
-				squares++;
-				last = element.id;
-			}
-			else if (element.id == last -1){
-				element.className = 'square2';
-				squares++;
-				last = element.id;
-				console.log(-1);
-			} 
-			else if (element.id == (last -(-8))){
-				element.className = 'square2';
-				squares++;
-				last = element.id;
-				console.log(+8);
-			}
-			else if (element.id == last -8){
-				element.className = 'square2';
-				squares++;
-				last = element.id;
-				console.log(-8);
-			}
+
+function submitTemplate(){
+	for (let index = 1; index <= 48; index++) {
+		var grid = document.getElementById(index);
+		if (grid.className == 'square2'){
+			let square = document.getElementById(index + 48);
+			square.className = 'square1';
+			grid.className = 'squareT';
 		}
-		else {
-			// console.log(element.id)
-			element.className = 'square2';
-			squares++;
-			last = element.id;
-		}
-		// squares++;
-		// element.className = 'square2';
-	} else if (element.className == 'square2') {
-		element.className = 'square1';
-		squares--;
 	}
-	// console.log(squares);
 }
 
-// Comment bloquer les cases ?
-// Si, détection d'une case avec square2, alors seules les cartes avec un id égal à
-// id + 1, id - 1, id + 8, id - 8 peuvent être cliquées
 
-// Essai d'une fonction qui vérifie si une case est déjà cliquée
-// function isDifferent() {
-// 	var grid = document.getElementsByClassName('grid');
-// 	for (i = 0; i < grid.length; i++) {
-// 		if (grid[i].className == 'square2') {
-// 			return grid[i].id;
-// 		}
-// 	}
-// }	
+document.addEventListener('keydown', function(e) {
+	let code = e.code;
+	if (code == 'ArrowRight'&& document.getElementById(8).className != 'square2' && document.getElementById(16).className != 'square2' && document.getElementById(24).className != 'square2' && document.getElementById(32).className != 'square2' && document.getElementById(40).className != 'square2' && document.getElementById(48).className != 'square2'){
+		for (let index = 47; index >= 1; index--) {
+			var grid = document.getElementById(index);
+			if (grid.className == 'square2'){
+				let square = document.getElementById(index +1);
+				square.className = 'square2';
+				grid.className = 'square1';
+			}
+		}
+	}
+	else if (code == 'ArrowLeft' && document.getElementById(1).className != 'square2' && document.getElementById(9).className != 'square2' && document.getElementById(17).className != 'square2' && document.getElementById(25).className != 'square2' && document.getElementById(33).className != 'square2' && document.getElementById(41).className != 'square2'){
+		for (let index = 1; index <= 48; index++) {
+			var grid = document.getElementById(index);
+			if (grid.className == 'square2'){
+				let square = document.getElementById(index -1);
+				square.className = 'square2';
+				grid.className = 'square1';
+			}
+		}
+	}
+	else if (code == 'ArrowUp' && document.getElementById(1).className != 'square2' && document.getElementById(2).className != 'square2' && document.getElementById(3).className != 'square2' && document.getElementById(4).className != 'square2' && document.getElementById(5).className != 'square2' && document.getElementById(6).className != 'square2' && document.getElementById(7).className != 'square2' && document.getElementById(8).className != 'square2'){
+		for (let index = 1; index <= 48; index++) {
+			var grid = document.getElementById(index);
+			if (grid.className == 'square2'){
+				let square = document.getElementById(index -8);
+				square.className = 'square2';
+				grid.className = 'square1';
+			}
+		}
+	}
+	else if (code == 'ArrowDown' && document.getElementById(41).className != 'square2' && document.getElementById(42).className != 'square2' && document.getElementById(43).className != 'square2' && document.getElementById(44).className != 'square2' && document.getElementById(45).className != 'square2' && document.getElementById(46).className != 'square2' && document.getElementById(47).className != 'square2' && document.getElementById(48).className != 'square2'){
+		for (let index = 40; index >= 1; index--) {
+			var grid = document.getElementById(index);
+			if (grid.className == 'square2'){
+				let square = document.getElementById(index +8);
+				square.className = 'square2';
+				grid.className = 'square1';
+			}
+		}
+	}
+});
 
+// Calls
 
-// Essai d'une fonction qui bloque les cases
-// function block() {
-// 	var grid = document.getElementsByClassName('grid');
-// 	for (i = 0; i < grid.length; i++) {
-// 		if (grid[i].id == isDifferent() + 1 || grid[i].id == isDifferent() - 1 || grid[i].id == isDifferent() + 8 || grid[i].id == isDifferent() - 8) == false {
-// 			grid[i].setAttribute('onclick', 'null');
-// 		}
-// 	}
-// }
+element.addEventListener('change', displayTemplate);
