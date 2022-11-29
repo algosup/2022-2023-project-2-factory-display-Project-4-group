0418af392a0468c407cc84737f3e1570e7b469dc
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-11-29 15:45:11 +0100

    working weather system

--- public/App/js/index.js
+++ public/App/js/index.js
@@ -6,10 +6,21 @@ function getCurrentTimeDate() {
 	var currentTime = `${hours}:${minutes}`;
 	document.getElementById('time').innerHTML = currentTime;
 	setTimeout(getCurrentTimeDate, 500);
+	if (minutes == 00 || minutes == 30) {
+		GetWeather();
+	}
 }
 function GetWeather() {
-	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-	setTimeout(GetWeather, 1200000);
+	!function(d,s,id){
+		var js,fjs=d.getElementsByTagName(s)[0];
+		if(!d.getElementById(id)){
+			js=d.createElement(s);
+			js.id=id;
+			js.src='https://weatherwidget.io/js/widget.min.js';
+			fjs.parentNode.insertBefore(js,fjs);
+		}
+	}
+	(document,'script','weatherwidget-io-js');
 }
 getCurrentTimeDate();
 GetWeather();
