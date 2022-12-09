2de2ea0454af249ec70299331b3d6078cd1411ae
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-12-09 16:15:03 +0100

    working code.js

--- public/App/js/code.js
+++ public/App/js/code.js
@@ -1,8 +1,8 @@
-import { initializeFirebase } from "./firebase.js";
-import { getFirestore, getDocs, collection, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+// import { initializeFirebase } from "./firebase.js";
+// import { getFirestore, getDocs, collection, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 
-var app = initializeFirebase();
-var db = getFirestore(app);
+// var app = initializeFirebase();
+// var db = getFirestore(app);
 
 var randomCode = Math.floor(Math.random() * 99999);
 var element = document.querySelector("#random-code");
