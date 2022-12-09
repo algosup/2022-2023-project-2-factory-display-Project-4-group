4f7d248fd8ce86b625cfabdd4d6dd9201bdcf005
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2022-12-09 14:21:48 +0100

    Creation of the code page

---
+++ public/App/js/code.js
@@ -0,0 +1,9 @@
+import { initializeFirebase } from "./firebase.js";
+import { getFirestore, getDocs, collection, setDoc, doc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+
+var app = initializeFirebase();
+var db = getFirestore(app);
+
+var randomCode = Math.floor(Math.random() * 99999);
+var element = document.querySelector("#random-code");
+element.innerHTML = randomCode;
