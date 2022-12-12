1093ecbd0f4a22a1151d9d6aad1dd1e4cc4d33e1
laurentbouquin <71769489+laurentbouquin@users.noreply.github.com>
2022-12-12 18:18:39 +0100

    Update screens page.

--- public/js/screens.js
+++ public/js/screens.js
@@ -8,26 +8,27 @@ var db = getFirestore(app);
 let screens = document.getElementById("screens")
 let screensNbr = 10;
 let screensArray = new Array;
+
 function createScreensPage() {
-	getDocs(collection(db, "Screens")).then((querySnapshot) => {
-		screensNbr = querySnapshot.size -1;
-		querySnapshot.forEach((doc) => {
-			screensArray.push({'name': doc.data().name, 'id': doc.data().id, 'restricted': doc.data().restricted, 'templateID': doc.data().templateID});
-		});
-	}).then(() => {
-		console.log(screensNbr);
-		for (let i = 0; i < screensNbr; i++) {
-			var div = document.createElement("div");
-			div.className = "screen";
-			div.innerHTML = " ";
-			if (screensArray[i].restricted == true) {
-				div.style.border = "3px solid #a00000";
-			} else {
-				div.style.border = "3px solid #00c000";
-			}
-			screens.appendChild(div);
-		}
-	});
+    getDocs(collection(db, "Screens")).then((querySnapshot) => {
+        screensNbr = querySnapshot.size - 1;
+        querySnapshot.forEach((doc) => {
+            screensArray.push({ 'name': doc.data().name, 'id': doc.data().id, 'restricted': doc.data().restricted, 'templateID': doc.data().templateID });
+        });
+    }).then(() => {
+        console.log(screensNbr);
+        for (let i = 0; i < screensNbr; i++) {
+            var div = document.createElement("div");
+            div.className = "screen";
+            div.innerHTML = screensArray[i].name;
+            if (screensArray[i].restricted == true) {
+                div.style.border = "3px solid #a00000";
+            } else {
+                div.style.border = "3px solid #00c000";
+            }
+            screens.appendChild(div);
+        }
+    });
 }
 
 createScreensPage();
